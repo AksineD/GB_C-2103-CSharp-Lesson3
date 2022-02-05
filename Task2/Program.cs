9cabb5d35476f8f19323f6fd23bb8ab13d9e1eaf
Denis Y <dyampolsky@yokohama-oht.com>
2022-02-05 16:43:11 +0200

    HW

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Phonebook");
+
+var telBook = new string[5][,]
+                  {
+                      new[,] { { "name1", "050", "name1@mail.com" } }, 
+                      new[,] { { "name2", "051", "name2@mail.com" } },
+                      new[,] { { "name3", "052", "name3@mail.com" } }, 
+                      new[,] { { "name4", "053", "name4@mail.com" } },
+                      new[,] { { "name5", "054", "name5@mail.com" } }
+                  };
+
+for (var i = 0; i < telBook.Length; i++)
+{
+    var userData = string.Empty;
+    if (telBook[i] != null && telBook[i].GetType() == typeof(string[,]))
+    {
+        userData = $"{i + 1}. ";
+        for (var j = 0; j < telBook[i].Length; j++) userData += telBook[i][0, j] + " ";
+    }
+    else
+    {
+        userData = "record is empty at given index";
+    }
+
+    Console.WriteLine(userData);
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿string GetUserInput()
+{
+    Console.WriteLine("Enter string");
+    return Console.ReadLine();
+}
+
+void ReversString(string inputStr)
+{
+    for (var i = inputStr.Length - 1; i >= 0; i--) Console.Write(inputStr[i]);
+}
+
+var str = GetUserInput();
+ReversString(str);
